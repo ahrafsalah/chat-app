432cce15cfc60e8c1a5a04082f1029507e42282c
unknown <ashrafsalah246@icloud.com>
2026-05-10 19:41:45 +0300

    frist commit

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["https://chat-app-lilac-nine.vercel.app", process.env.CLIENT_URL,"http://localhost:3000"],
     credentials: true,
   },
 });
