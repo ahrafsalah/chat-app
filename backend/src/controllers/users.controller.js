08c12384ece72375b8f77bdbead49fc83ef0cc1e
unknown <ashrafsalah246@icloud.com>
2026-05-11 19:39:05 +0300

    frist commit

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -73,7 +73,12 @@ const signIn = asyncHandler(async (req, res) => {
 });
 
 const logout = asyncHandler(async (req, res) => {
-  res.cookie("token", "", { maxAge: 0 });
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    path: "/",
+  });
   res.status(200).json({ message: "User logged out successfully" });
 });
 
